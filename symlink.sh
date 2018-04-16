fff3c3235da6220c717a57b593d972d0dceb4831
Vince Au <vinceau09@gmail.com>
2018-04-16 12:19:00 +1000

    Fix the symlink for nvim/init.vim

--- symlink.sh
+++ symlink.sh
@@ -10,7 +10,7 @@ ln -s $p/.profile ~
 ln -s $p/.pylintrc ~
 ln -s $p/.vimperatorrc ~
 ln -s $p/.vimrc ~
-ln -s $p/init.vim "$XDG_CONFIG_HOME/nvim/init.vim"
+ln -s $p/.vimrc "$XDG_CONFIG_HOME/nvim/init.vim"
 
 git config --global core.excludesfile ~/.gitignore_global
 echo -e "source ~/.profile\nsource ~/.bashrc" >> ~/.bash_profile
